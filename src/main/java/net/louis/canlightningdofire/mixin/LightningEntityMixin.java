b987a3d77f5a968dd525801994f35056d25f5fbf
RodoFire <louisbriche@gmail.com>
2024-05-01 20:03:00 +0200

    updated to 1.20.6

--- src/main/java/net/louis/canlightningdofire/mixin/LightningEntityMixin.java
+++ src/main/java/net/louis/canlightningdofire/mixin/LightningEntityMixin.java
@@ -40,20 +40,16 @@ public abstract class LightningEntityMixin extends Entity {
      */
     @Overwrite
     private void spawnFire(int spreadAttempts) {
-        System.out.println("ok ok ok ok okok oko kok ok ok ok ok o kok ok ok ok ok ");
         if (this.cosmetic || this.getWorld().isClient || !this.getWorld().getGameRules().getBoolean(GameRules.DO_FIRE_TICK) || !this.getWorld().getGameRules().getBoolean(CanLightningDoFire.LIGHTNING_DO_FIRE)) {
-            System.out.println("nooo oo o oo o oo o o o oo o on");
             return;
         }
         BlockPos blockPos = this.getBlockPos();
         BlockState blockState = AbstractFireBlock.getState(this.getWorld(), blockPos);
         if (this.getWorld().getBlockState(blockPos).isAir() && blockState.canPlaceAt(this.getWorld(), blockPos)) {
-            System.out.println("pro");
             this.getWorld().setBlockState(blockPos, blockState);
             ++this.blocksSetOnFire;
         }
         for (int i = 0; i < spreadAttempts; ++i) {
-            System.out.println("pro");
             BlockPos blockPos2 = blockPos.add(this.random.nextInt(3) - 1, this.random.nextInt(3) - 1, this.random.nextInt(3) - 1);
             blockState = AbstractFireBlock.getState(this.getWorld(), blockPos2);
             if (!this.getWorld().getBlockState(blockPos2).isAir() || !blockState.canPlaceAt(this.getWorld(), blockPos2)) continue;
